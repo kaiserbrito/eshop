e420b2c5e93f2fd5ca7782dc26f445d7e578b2f0
Victor Guerra <victorbcguerra@gmail.com>
2017-11-15 18:01:07 -0200

    creating Product model

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.Home.as_view()),
+  url(r'^products/$', views.product_list, name='product_list'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import Product
 
 # Create your views here.
 
 class Home(View):
   def get(self, request, *args):
     return render (request, 'home.html')
+
+def product_list(request):
+  products = Product.objects.all()
+  return render(request, 'products/product_list.html', {'products': products})
